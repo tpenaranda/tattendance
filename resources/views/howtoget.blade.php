9ed31711d9ab83f43905b813c46a57f2b21be707
Tate Peñaranda <tate@xenir.com.ar>
2016-03-09 13:40:09 -0300

    Translation for howtoget template.

--- resources/views/howtoget.blade.php
+++ resources/views/howtoget.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>¿Cómo llegar?</h1>
+<h1>How to get???</h1>
 <h4>(-33.106732, -60.529882 - General Lagos)</h4>
 <br>
 <br>
-<p>Yendo por la Ruta 21, en la entrada al camino de ripio que los depositará en la casa,<br>podrán ver algunos globitos de colores que les harán saber que van en el rumbo correcto.</p>
+<p>
+    Take highway 21, look for a long and narrow gravel road,<br>
+    you will se many colored ballons indicating the way to the party!.
+</p>
 <br>
 <h5>
-    <a href="https://www.google.com.ar/maps/place/33%C2%B006'24.2%22S+60%C2%B031'47.6%22W/@-33.1065536,-60.5328088,532m/data=!3m1!1e3!4m2!3m1!1s0x0:0x0" target="_blank">Link de Google Maps</a>
+    <a href="https://www.google.com.ar/maps/place/33%C2%B006'24.2%22S+60%C2%B031'47.6%22W/@-33.1065536,-60.5328088,532m/data=!3m1!1e3!4m2!3m1!1s0x0:0x0" target="_blank">Google Maps Link</a>
 </h5>
 <br>
-<h4>Mapas</h4>
+<h4>Maps</h4>
 <div style="border: 2px solid; width: 530px;">
     <img src="/static/img/map0.png" border="5">
 </div>
